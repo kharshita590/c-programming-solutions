0a0f13f377690e8669c9ee5f2d5e9fc2de1befbf
kharshita590 <83570759+kharshita590@users.noreply.github.com>
2025-01-03 18:22:07 +0530

    Update solutions.c

--- solutions.c
+++ solutions.c
@@ -148,3 +148,24 @@ int main(){
         printf("no not equal\n");
     }
 }
+
+
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+    char ch[100],temp;
+    printf("Enter string : ");
+    scanf("%s",ch);
+    int i,j;
+    i=0;
+    j=strlen(ch)-1;
+    while(i<j){
+        temp=ch[i];
+        ch[i]=ch[j];
+        ch[j]=temp;
+        i++;
+        j--;
+    }
+    printf("Reversed %s",ch);
+}
