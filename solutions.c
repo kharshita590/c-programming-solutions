0defcfc488b54999a25397c9a50f8d918c3d56be
kharshita590 <83570759+kharshita590@users.noreply.github.com>
2025-01-03 19:28:19 +0530

    Update solutions.c

--- solutions.c
+++ solutions.c
@@ -199,3 +199,40 @@ int main(){
         printf("palindrome");
     }
 }
+
+
+
+
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+    char names[100][100];
+    int n;
+    
+    printf("Enter the number of names : ");
+    scanf("%d",&n);
+    getchar();
+    
+    printf("Enter the names: \n");
+    for(int i=0;i<n;i++){
+        fgets(names[i],sizeof(names[i]),stdin);
+        names[i][strcspn(names[i],"\n")]='\0';
+    }
+    
+    for(int i=0;i<n-1;i++){
+        for(int j=0;j<n-i-1;j++){
+            if(strcmp(names[j],names[j+1])>0){
+                char temp[100];
+                strcpy(temp,names[j]);
+                strcpy(names[j],names[j+1]);
+                strcpy(names[j+1],temp);
+            }
+        }
+    }
+    printf("Sorted names in alphabetical orders : ");
+    for(int i=0;i<n;i++){
+        printf("%s\n",names[i]);
+    }
+    
+}
