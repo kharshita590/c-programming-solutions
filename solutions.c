13e24b9b1fc075b84ffb340ed72e05e007af4a2a
kharshita590 <83570759+kharshita590@users.noreply.github.com>
2025-01-03 15:46:25 +0530

    Update solutions.c

--- solutions.c
+++ solutions.c
@@ -70,3 +70,30 @@ void main(){
     str2[i]='\0';
     printf("The copied string : %s\n",str2);
 }
+
+
+#include <stdio.h>
+
+int main(){
+    char str1[100], str2[100],result[100];
+    int i=0,j=0;
+    
+    printf("Enter first string : ");
+    fgets(str1,sizeof(str1),stdin);
+    
+    printf("Enter second string : ");
+    fgets(str2,sizeof(str2),stdin);
+    
+    while(str1[i]!='\0'){
+        result[i]=str1[i];
+        i++;
+    }
+    
+    while(str2[j]!='\0'){
+        result[i]=str2[j];
+        i++;
+        j++;
+    }
+    result[i]='\0';
+    printf("The given string : %s\n",result);
+}
